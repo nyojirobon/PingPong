4fccfeadfc30fe61b978bd73ad857d7fd6edd706
YN <yo.z940127@gmail.com>
2018-03-26 00:43:49 +0900

    make agent

--- train/environment.py
+++ train/environment.py
@@ -5,21 +5,25 @@ import numpy as np
 class Environment:
     """
     Virtual environment of ping pong for learning.
+    'done' is not defined, because if one lost of rally means lost of the game,
+    learning is not going well.
+    Thus, agent should set maximum timesteps for each episode.
     """
+
+    # 0:stay, 1:left, 2:right
+    action_space = np.array([0,1,2])
 
     def __init__(self):
         self.field_width = 600
         self.field_height = 400
         self.ball_radius = 10
-        self.paddle_width = 80
+        self.paddle_width = 60
         self.paddle_height = 10
-        self.done = false # flag for if game is over
         self.ball_x = self.field_width/2 # ball position(x-coordinate)
         self.ball_y = self.field_height/2 # ball position(y-coordinate)
-        self.ball_dx = 3 # ball moving speed(x-axis)
-        self.ball_dy = -3 # ball moving speed(y-axis)
-        self.paddle_x = (self.field_width - self.paddleWidth)/2 # agent's paddle position(x-coordinate)
-        self.opponent_paddle_x = (self.field_width - self.paddleWidth)/2 # opponet's paddle position(x-coordinate)
+        self.ball_dx = np.random.choice([5, -5]) # ball moving speed(x-axis)
+        self.ball_dy = np.random.choice([5, -5]) # ball moving speed(y-axis)
+        self.paddle_x = (self.field_width - self.paddle_width)/2 # agent's paddle position(x-coordinate)
 
     def get_reward(self):
         """
@@ -30,70 +34,57 @@ class Environment:
         """
         if self.ball_x + self.ball_dx > self.field_width - self.ball_radius or \
         self.ball_x + self.ball_dx < self.ball_radius:
-            self.ball_dx -= self.ball_dx
+            self.ball_dx = -self.ball_dx
 
-        if self.ball_y + self.ball_dy > self.field_height - self.ball_radius:
-            if self.ball_x + self.ball_radius > self.opponent_paddle_x and \
-            self.ball_x - self.ball_radius < self.opponent_paddle_x + self.paddle_width:
-                self.ball_dy -= self.ball_dy
-                return 1.0
-            else: # Win the game
-                self.done = true
-                return 100.0
-        elif self.ball_y + self.ball_dy < self.ball_radius:
+        if self.ball_y + self.ball_dy < self.ball_radius:
             if self.ball_x + self.ball_radius > self.paddle_x and \
             self.ball_x - self.ball_radius < self.paddle_x + self.paddle_width:
-                self.ball_dy -= self.ball_dy
+                self.ball_dy = -self.ball_dy
                 return 1.0
-            else: # Lose the game
-                self.done = true
-                return -100.0
+            else: # Lose this rally
+                self.ball_dy = -self.ball_dy
+                return -200.0
+        elif self.ball_y + self.ball_dy > self.field_height - self.ball_radius:
+            self.ball_dy = -self.ball_dy
+            return 1.0
         else:
             return 1.0
 
-    def move_paddle(self, current_pos, direction):
+    def move_paddle(self, direction):
         """
         Move paddle toward given direction.
 
         # Arguments
-            current_pos(Integer) : current x-coordinate of paddle
-            direction(Integer) : direction of paddle movement(0:stay, 1:left, 2:right)
+            direction(int) : direction of paddle movement
         """
         if direction == 1: # move to left
-            if current_pos - 10 >= 0:
-                return current_pos - 10
+            if self.paddle_x - 10 > 0:
+                self.paddle_x -= 10
             else:
-                return 0
+                self.paddle_x =  0
         elif direction == 2: # move to right
-            if current_pos + 10 <= self.field_width - self.paddle_width:
-                return current_pos + 10
+            if self.paddle_x + 10 < self.field_width - self.paddle_width:
+                self.paddle_x += 10
             else:
-                return self.field_width - self.paddle_width
-        else: # stay at current position
-            return current_pos
+                self.paddle_x = self.field_width - self.paddle_width
 
     def step(self, direction):
         """
         Run one timestep of the environment's dynamics.
 
         # Arguments
-            direction(Integer) : direction of paddle movement(0:stay, 1:left, 2:right)
+            direction(int) : direction of paddle movement
 
         # Returns:
             observation(object) : observation of the environment from agent
             reward(float) : immediate reward for current step
-            done(boolean) : whether the current game has ended
         """
-        if self.done:
-            raise Exception("The game has already ended, should call 'reset()'")
-
+        self.move_paddle(direction)
         reward = self.get_reward()
-        self.paddle_x = self.move_paddle(self.paddle_x, direction)
-        self.opponent_paddle_x = self.move_paddle(self.opponent_paddle_x, np.random.randint(3))
         self.ball_x += self.ball_dx
         self.ball_y += self.ball_dy
-        observation = (self.paddle_x, self.opponent_paddle_x, self.ball_x, self.ball_y)
-        return (observation, reward, self.done)
+        observation = (self.paddle_x, self.ball_x, self.ball_y, self.ball_dx, self.ball_dy)
+        return (observation, reward)
 
     def reset(self):
         """
@@ -102,11 +93,9 @@ class Environment:
         # Returns:
             observation(object) : initial observation
         """
-        self.done = false
         self.ball_x = self.field_width/2
         self.ball_y = self.field_height/2
-        self.ball_dx = 3
-        self.ball_dy = -3
-        self.paddle_x = (self.field_width - self.paddleWidth)/2
-        self.opponent_paddle_x = (self.field_width - self.paddleWidth)/2
-        return (self.paddle_x, self.opponent_paddle_x, self.ball_x, self.ball_y)
+        self.ball_dx = np.random.choice([5, -5])
+        self.ball_dy = np.random.choice([5, -5])
+        self.paddle_x = (self.field_width - self.paddle_width)/2
+        return (self.paddle_x, self.ball_x, self.ball_y, self.ball_dx, self.ball_dy)
---
+++ train/train.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import os
+from environment import Environment
+
+def normalise_state(observation):
+    """
+    Normalise each element of state.
+
+    # Arguments
+        observation(object) : current observation of the environment from agent
+
+    # Returns
+        normalised state(int) : state normalised in each element
+    """
+    paddle_pos, ball_x, ball_y, ball_dx, ball_dy = observation
+    normalised_state = [
+        np.digitize(paddle_pos, bins = np.linspace(0, 540, normalise_num + 1)[1:-1]),
+        np.digitize(ball_x, bins = np.linspace(0, 600, normalise_num + 1)[1:-1]),
+        np.digitize(ball_y, bins = np.linspace(0, 400, normalise_num + 1)[1:-1]),
+        np.digitize(ball_dx, bins = np.linspace(-5, 5, normalise_num + 1)[1:-1]),
+        np.digitize(ball_dy, bins = np.linspace(-5, 5, normalise_num + 1)[1:-1])
+    ]
+    return sum([x * (normalise_num**i) for i, x in enumerate(normalised_state)])
+
+def get_action(action_space, state, episode):
+    """
+    Choose action in given state by 'decaying ε-greedy' policy.
+
+    # Arguments
+        action_space(numpy.ndarray) : choosable actions
+        state(int) : current state
+        episode() : number of current episode
+
+    # Returns
+        action(int) : action to take in current state
+    """
+    epsilon = 0.5 * (1 / (episode + 1))
+    if epsilon <= np.random.uniform(0, 1):
+        action = np.argmax(q_table[state]) # choose best action
+    else:
+        action = np.random.choice(action_space) # choose random action
+    return action
+
+def update_qtable(q_table, state, action, reward, next_state):
+    """
+    Update Q-table.
+
+    # Arguments
+        q_table(numpy.ndarray) : current Q-table
+        state(int) : current state
+        action(int) : action to take in current state
+        reward(float) : immediate reward of this timestep
+        next_state(int) : next state
+
+    # Returns
+        q_table(numpy.ndarray) : updated Q-table
+    """
+    discount_factor = 0.99 # discount future reward
+    learning_rate = 0.5
+    next_max_Q = max(q_table[next_state][0], q_table[next_state][1], q_table[next_state][2])
+    q_table[state][action] = (1 - learning_rate) * q_table[state][action] +\
+     learning_rate * (reward + discount_factor * next_max_Q)
+    return q_table
+
+# Main Operation
+env = Environment()
+max_number_of_steps = 2000 # maximum number of steps per episode
+num_episodes = 2000
+normalise_num = 10 # separate each element of state into 10
+q_table = np.random.uniform(low= -1, high= 1, size= (normalise_num**5, env.action_space.size))# create Q table
+
+for episode in range(num_episodes):
+    observation = env.reset()
+    state = normalise_state(observation)
+    action = np.argmax(q_table[state])
+    episode_reward = 0
+
+    for t in range(max_number_of_steps):
+        # Take one timestep in the environment
+        observation, reward = env.step(action)
+        episode_reward += reward
+        next_state = normalise_state(observation)
+
+        q_table = update_qtable(q_table, state, action, reward, next_state)# update Q table
+        action = get_action(env.action_space, next_state, episode)
+        state = next_state
+
+    print("In %dth episode, episode reward is %f." % (episode, episode_reward))
+
+# Save Q table
+pardir =os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+q_table_path = os.path.join(pardir, "predict", "qtable.csv")
+np.savetxt(q_table_path, q_table, delimiter=",")
